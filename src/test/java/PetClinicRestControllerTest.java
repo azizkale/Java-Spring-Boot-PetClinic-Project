8295d65ecd59f8f1d66d567b06c7df670bbecaa8
azizkale <azizkale@hotmail.com>
2023-07-20 12:34:07 +0200

    test-testGetOwnersByLastName func added

--- src/test/java/PetClinicRestControllerTest.java
+++ src/test/java/PetClinicRestControllerTest.java
@@ -6,6 +6,10 @@ import org.junit.Test;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class PetClinicRestControllerTest {
 
     private RestTemplate restTemplate;
@@ -20,5 +24,16 @@ public class PetClinicRestControllerTest {
         ResponseEntity<Owner> response = restTemplate.getForEntity("http://localhost:8080/rest/owner/1", Owner.class);
         MatcherAssert.assertThat(response.getStatusCodeValue(), Matchers.equalTo(200));
         MatcherAssert.assertThat(response.getBody().getFirstName(), Matchers.equalTo("Kenan"));
+    }
+
+    @Test
+    public void testGetOwnersByLastName(){
+        ResponseEntity<List> response = restTemplate.getForEntity("http://localhost:8080/rest/owner?ln=Sevindik",List.class);
+        MatcherAssert.assertThat(response.getStatusCodeValue(),Matchers.equalTo(200));
+        List<Map<String,String>> body = response.getBody();
+        List<String> firstNames = body.stream().map(e-> e.get("firstName")).collect(Collectors.toList());
+
+        MatcherAssert.assertThat(firstNames,Matchers.containsInAnyOrder("Kenan","Hümeyra","Salim"));
+        System.out.println(firstNames);
     }
 }
