308bc62637570d670516439e7c5ba2fcc965c0e4
azizkale <azizkale@hotmail.com>
2023-07-20 19:42:32 +0200

    PetClinicRestController class - request POST method added

--- src/main/java/com/javaegitimleri/petclinic/web/PetClinicRestController.java
+++ src/main/java/com/javaegitimleri/petclinic/web/PetClinicRestController.java
@@ -3,10 +3,14 @@ package com.javaegitimleri.petclinic.web;
 import com.javaegitimleri.petclinic.exception.OwnerNotFoundException;
 import com.javaegitimleri.petclinic.model.Owner;
 import com.javaegitimleri.petclinic.service.PetClinicService;
+
+import java.net.URI;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RequestMapping("/rest")
 @RestController
@@ -14,6 +18,19 @@ public class PetClinicRestController {
 
     @Autowired
     private PetClinicService petClinicService;
+
+    @RequestMapping(method = RequestMethod.POST, value="owner")
+    public ResponseEntity<Owner> createOwner(@RequestBody Owner owner){
+        try {
+            petClinicService.createOwner(owner);
+            Long id = owner.getId();
+            URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(id).toUri();
+            return ResponseEntity.created(location).build();
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+
+    }
 
     @RequestMapping(method = RequestMethod.GET, value="/owners")
     public ResponseEntity<List<Owner>> getOwners(){
